5126732355701da21cbee32142c199bf0cfe189e
Rysiek Olkusnik <rysiek2000@gmail.com>
2016-11-27 18:13:54 +0100

    refactor app class and update README with benchmarks

--- src/com/grid/test/CountApp.java
+++ src/com/grid/test/CountApp.java
@@ -1,56 +1,46 @@
 package com.grid.test;
 
 public class CountApp {
-
+	PersonTable personTable;
+	static final long[] MASK1={1L,2L,4L,8L,16L,32L,64L,0L};
+	static final long[] MASK2={0L,0L,0L,0L,0L,0L,0L,1L};
+	long start;
+	
 	public static void main(String[] args) {
-		PersonTable personTable = generate();	
-		countWith64Attrs(personTable);
-		countWith64Attrs(personTable);
-		countWith500Attrs(personTable);		
-		countWith500Attrs(personTable);		
-		countWith500AttrsPesymistic(personTable);		
+		CountApp app = new CountApp();
+		app.generate(10000000, 500);	
+		app.countAttrs(MASK1, 1);//The first time is slower		
+		app.countAttrs(MASK2, 100);		
+		app.countAttrs(MASK1, 100);		
 	}
 
-	private static PersonTable generate() {
-		int recCount=10000000;
-		System.out.println("Generating sample Data with "+recCount+"...");
-		long start = System.currentTimeMillis();
-		PersonTable personTable = DataGenerator.GeneratePersonTable(recCount, 500);
-		long time = System.currentTimeMillis() - start;
-		System.out.println("Generated in " + time + " ms.");
-		return personTable;
+	private void generate(int recCount, int attrCount) {
+		System.out.println("Generating sample Data with "+recCount+" recs and " + attrCount + " attrs...");
+		before();
+		personTable = DataGenerator.GeneratePersonTable(recCount, attrCount);
+		System.out.println("Generated in " + after() + " ms.");
 	}
 
-	private static void countWith64Attrs(PersonTable personTable) {
-		long mask=1500L;
+	private void countAttrs(long[] mask, int loop) {
 		
-		System.out.println("Counting with max 64 attrs...");
-		long start = System.currentTimeMillis();
-		long count = personTable.parallelStream().filter(person -> person.checkMask0(mask) ).count();
-		long time = System.currentTimeMillis() - start;
-		System.out.println("found " + count + " recs");
-		System.out.println("Counted in " + time + " ms.");
+		System.out.print("Counting with max " + personTable.Get(0).getAttrs().length*64 + " attrs. Loop: " + loop);
+		before();
+		long count=0L;
+		long run=loop;
+		do{
+			count = personTable.stream().filter(person -> person.checkMask8(mask) ).count();
+		}while (--run > 0);
+		long time = after();
+		long avg = time/(long)loop;
+		System.out.println(" Found " + count + " recs in \t" + time + " ms. (average: "+ avg +" ms.)");
 	}
-
-	private static void countWith500Attrs(PersonTable personTable) {
-		long[] mask={1L,2L,4L,8L,16L,32L,64L,0L};
-		
-		System.out.println("Counting with max 500 attrs...");
-		long start = System.currentTimeMillis();
-		long count = personTable.parallelStream().filter(person -> person.checkMask8(mask) ).count();
-		long time = System.currentTimeMillis() - start;
-		System.out.println("found " + count + " recs");
-		System.out.println("Counted in " + time + " ms.");
+	
+	private void before(){
+		start = System.currentTimeMillis();
 	}
-	private static void countWith500AttrsPesymistic(PersonTable personTable) {
-		long[] mask={0L,0L,0L,0L,0L,0L,0L,1L};
-		
-		System.out.println("Counting with max 500 attrs pesymistic...");
-		long start = System.currentTimeMillis();
-		long count = personTable.parallelStream().filter(person -> person.checkMask8(mask) ).count();
-		long time = System.currentTimeMillis() - start;
-		System.out.println("found " + count + " recs");
-		System.out.println("Counted in " + time + " ms.");
+	
+	private long after(){
+		return System.currentTimeMillis() - start;
 	}
 
 }
